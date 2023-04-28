fd4187cac89cab09d6357bb4fbd0d89e64a8409e
karthick <karthickkumar7.k7@gmail.com>
2023-04-28 15:56:33 -0700

    fixed bug

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -8,7 +8,7 @@ const Cell = ({ num, flag, pos, enterVal }) => {
 
     return (
         <input
-            className={`w-[36px] h-[38px] md:w-[67.5px] md:h-[67.5px] md:px-5 text-xl text-center md:text-2xl border-gold text-gold bg-slate-800 font-bold cursor-pointer outline-none hover:bg-slate-700 focus:bg-gold/40 duration-200 ${
+            className={`w-[36px] h-[38px] md:w-[67px] md:h-[67px] md:px-5 text-xl text-center md:text-2xl border-gold text-gold bg-slate-800 font-bold cursor-pointer outline-none hover:bg-slate-700 focus:bg-gold/ duration-200 ${
                 styles[pos] && styles[pos]
             }`}
             value={num}
